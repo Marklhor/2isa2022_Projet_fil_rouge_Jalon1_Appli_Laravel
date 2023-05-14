f0930316ea71e3918b177b7340224b44de27cf35
marklhor <marklhor@hotmail.fr>
2023-05-14 15:29:36 +0200

    commit, test import js via vite, ne fonctionne pas

--- fil-rouge/resources/js/app.js
+++ fil-rouge/resources/js/app.js
@@ -1,1 +1,5 @@
 import './bootstrap';
+import '../css/app.css';
+
+
+
--- fil-rouge/resources/views/connexion.blade.php
+++ fil-rouge/resources/views/connexion.blade.php
@@ -5,8 +5,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <link rel="stylesheet" href="{{URL::asset('css/style.css')}}">
+    <link rel="stylesheet" href="{{ Vite::asset('resources/js/app.js') }}">
+    <link rel="stylesheet" href="{{ Vite::asset('resources/css/app.css') }}">
+    <!-- <link rel="stylesheet" href="{{URL::asset('css/style.css')}}"> -->
     <title>Elements du site</title>
    
 </head>
