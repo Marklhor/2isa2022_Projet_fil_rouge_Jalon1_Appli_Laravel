3312f4a433d4af49e041ddc5ac8f50214dddc4d0
marklhor <marklhor@hotmail.fr>
2023-05-15 15:18:40 +0200

    commit, début des règles de vérifications dans incidentRequest.php

--- fil-rouge/app/Http/Requests/IncidentRequest.php
+++ fil-rouge/app/Http/Requests/IncidentRequest.php
@@ -11,7 +11,7 @@ class IncidentRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,8 @@ class IncidentRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'sujet' => ['required', 'string', 'max:100'],
+            'idStatus' => ['required', 'integer', 'numeric']
         ];
     }
 }
