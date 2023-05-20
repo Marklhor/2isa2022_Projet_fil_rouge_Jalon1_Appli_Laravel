6ed1d0d3dbe911c17a6d525f331be6c58b9f029c
marklhor <marklhor@hotmail.fr>
2023-05-20 18:13:19 +0200

    idem

--- fil-rouge/app/Models/Post.php
+++ fil-rouge/app/Models/Post.php
@@ -1,22 +1,26 @@
 <?php
 namespace App;
+
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Casts\Attribute;
+
 class Post extends Model
 {
     /**
-* The table associated with the model.
-*
-* @var string
-*/
+     * The table associated with the model.
+     *
+     * @var string
+     */
     protected $table = 'posts';
     /**
-* The attributes that are mass assignable.
-*
-* @var array
-*/
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = [
-        'post_content', 'post_topic', 'post_by'
+        'post_content',
+        'post_topic',
+        'post_by'
     ];
 }
 // TODO créer table et donnée
