ef83f241a4af5ce0369a9708d9fa1d960c04e60b
marklhor <marklhor@hotmail.fr>
2023-05-25 23:09:31 +0200

    commit, style chat

--- fil-rouge/resources/views/ticket.blade.php
+++ fil-rouge/resources/views/ticket.blade.php
@@ -2,6 +2,7 @@
 
 @section('contenu')
     @if (!empty($data))
+        {{-- <?php dd($data); ?> --}}
         <article>
             <section>
                 <detail>
@@ -12,7 +13,25 @@
                         </p>
                     </summary>
                     <div class="border" id="incident_detail">
-                        <p class="txt_inline">Statut : <span class="ticket_values">{{ $data[0]->status_label }}</span></p>
+                        <p class="txt_inline">
+                            Statut :
+                            <span class="ticket_values">
+                                {{ $data[0]->status_label }}
+                                @switch($data[0]->id_status)
+                                    @case(11111)
+                                        <img class="icon_advance" src="/img/red.svg" alt="">
+                                    @break
+
+                                    @case(22222)
+                                        <img class="icon_advance" src="/img/yellow.svg" alt="">
+                                    @break
+
+                                    @case(33333)
+                                        <img class="icon_advance" src="/img/green.svg" alt="">
+                                    @break
+                                @endswitch
+                            </span>
+                        </p>
                         <p class="txt_inline">
                             <label for="panne_type"></label>
                             Type de panne :
@@ -20,13 +39,13 @@
                                 {{ $data[0]->label_panne }}
                             </span>
                         </p>
-                        <p class="txt_inline">Date d’ouverture : <time
-                                class="ticket_values" datetime="{{ $data[0]->date_de_creation }}">{{ $data[0]->date_de_creation }}</time>
+                        <p class="txt_inline">Date d’ouverture : <time class="ticket_values"
+                                datetime="{{ $data[0]->date_de_creation }}">{{ $data[0]->date_de_creation }}</time>
                         </p>
-                        <p class="txt_inline">Dernière mise à jour : <time
-                                class="ticket_values" datetime="{{ $data[0]->date_de_maj }}">{{ $data[0]->date_de_maj }}</time>
+                        <p class="txt_inline">Dernière mise à jour : <time class="ticket_values"
+                                datetime="{{ $data[0]->date_de_maj }}">{{ $data[0]->date_de_maj }}</time>
                         </p>
-                        <p class="txt_inline">Sujet : <span class="ticket_values">{{ $data[0]->sujet }}</span>
+                        <p class="txt_inline sujet">Sujet : <span class="ticket_values">{{ $data[0]->sujet }}</span>
                         </p>
                     </div>
                 </detail>
@@ -42,7 +61,7 @@
                                 {{-- {{ var_bump($message) }} --}}
 
                                 <li @switch($message->id_user)
-                                    @case (session()->get('idUser'))
+                                    @case ($data[0]->id_user)
                                         class="message right">
                                         <img class="logo" src="/img/computer-icons-clip-art-man.svg" alt="usager">
                                         @break
@@ -53,13 +72,16 @@
                                     @endswitch
                                     <p>
                                     {{ $message->msg }}
-                                    <p>
-                                        @if ($message->id_user != session()->get('idUser'))
-                                            <span>{{ $message->nom }}</span>
-                                        @endif
-                                        <time datetime="{{ $message->date_message }}">{{ $message->date_message }}</time>
                                     </p>
-                                    </p>
+                                    <div class="date">
+                                        <p>
+                                            @if ($message->id_user != $data[0]->id_user)
+                                                <span>{{ $message->nom }} </span>
+                                            @endif
+                                            <time
+                                                datetime="{{ $message->date_message }}">{{ $message->date_message }}</time>
+                                        </p>
+                                    </div>
                                 </li>
                             @endforeach
                         </ul>
