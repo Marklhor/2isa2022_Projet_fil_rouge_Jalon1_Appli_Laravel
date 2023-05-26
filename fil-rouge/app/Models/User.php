db02998dfc4d07d67afb373be4dbaa677b7f9f36
marklhor <marklhor@hotmail.fr>
2023-05-26 12:29:53 +0200

    commit, model user

--- fil-rouge/app/Models/User.php
+++ fil-rouge/app/Models/User.php
@@ -43,5 +43,8 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    
+    public function getRole()
+    {
+        // requete sql en utilisant $this->id
+    }
 }
