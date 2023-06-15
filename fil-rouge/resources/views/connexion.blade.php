ef13ac56adf49e39fdd4303a39c0916cd70926d0
marklhor <marklhor@hotmail.fr>
2023-06-15 10:56:03 +0200

    commit, favicon

--- fil-rouge/resources/views/connexion.blade.php
+++ fil-rouge/resources/views/connexion.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.1">
-
+    @include('templates.favicon')
     <link rel="stylesheet" href="{{ Vite::asset('resources/css/style.css') }}">
     <title>Elements du site</title>
 
---
+++ fil-rouge/resources/views/templates/favicon.blade.php
@@ -0,0 +1,1 @@
+<link rel="icon" href="{{ URL::asset('/img/favicon-16x16.ico') }}" type="image/x-icon" />
