c5cdb6c08b57791038b1f00fcd88254cabd238b7
marklhor <marklhor@hotmail.fr>
2023-06-18 21:26:26 +0200

    commit, meta

--- fil-rouge/resources/views/templates/metas.blade.php
+++ fil-rouge/resources/views/templates/metas.blade.php
@@ -2,5 +2,5 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="author" content="Patrick Nardi">
-<meta name="description" content="Gestion des incidents du parc informatique">
-<meta name="keywords" content="incident, informatique">
+<meta name="description" content="AMIO 2isa CDA07 | Projet fil-rouge phase 1 | Gestion des incidents du parc informatique">
+<meta name="keywords" content="AMIO, 2isa, CDA07, fil-rouge, incident, informatique">
