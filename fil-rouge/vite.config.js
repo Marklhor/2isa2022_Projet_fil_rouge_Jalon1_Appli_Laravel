22773f8cc604528bf9510932edb7bcc3e9baa702
marklhor <marklhor@hotmail.fr>
2023-06-20 14:13:59 +0200

    commit, ajouter des fichier JS ou CSS pour Vite

--- fil-rouge/vite.config.js
+++ fil-rouge/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-// TODO ici pour ajouter des fichier JS ou CSS pour Vite
 export default defineConfig({ plugins: [laravel({ input: ['resources/css/style.css', 'resources/js/app.js', 'resources/js/liste_incidents.js', 'resources/js/register.js',], refresh: true, }),], });
 
 
