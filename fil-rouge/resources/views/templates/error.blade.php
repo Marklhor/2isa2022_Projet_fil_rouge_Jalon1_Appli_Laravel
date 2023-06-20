8deff72c73a76fc5a05a1e9c67e3c870d5ef5642
marklhor <marklhor@hotmail.fr>
2023-06-20 14:15:17 +0200

    commit, error blade, supp des commentaire, pas réussis à faire l'algo

--- fil-rouge/resources/views/templates/error.blade.php
+++ fil-rouge/resources/views/templates/error.blade.php
@@ -1,10 +1,4 @@
 <div class="error txt_inline">
-    {{-- // TODO algo impossible voir le TODO perte de l iduser de session dans TicketController --}}
-    {{-- @if (!empty(session()->get('error')))
-        {{ session()->get('error') }}
-    @elseif(!empty(session()->get('errordb')))
-        {{ session()->get('errordb') }}
-    @endif --}}
     {{ session()->get('error') }}
     {{ session()->get('errordb') }}
 </div>
