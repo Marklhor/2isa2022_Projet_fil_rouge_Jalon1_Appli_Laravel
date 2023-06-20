35940890d2c1c503e2d1fc2d1158d9e61f6dcd32
marklhor <marklhor@hotmail.fr>
2023-06-20 14:26:54 +0200

    commit, méta initial-scale=1.0 à initial-scale=1 pour voir si changement du zome sur différentes plateforme

--- fil-rouge/resources/views/templates/metas.blade.php
+++ fil-rouge/resources/views/templates/metas.blade.php
@@ -1,6 +1,7 @@
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="author" content="Patrick Nardi">
-<meta name="description" content="AMIO 2isa CDA07 | Projet fil-rouge phase 1 | Gestion des incidents du parc informatique">
+<meta name="description"
+    content="AMIO 2isa CDA07 | Projet fil-rouge phase 1 | Gestion des incidents du parc informatique">
 <meta name="keywords" content="AMIO, 2isa, CDA07, fil-rouge, incident, informatique">
