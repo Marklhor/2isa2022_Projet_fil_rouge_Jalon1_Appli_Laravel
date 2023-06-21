267ac9d9b9ad39eb0d2d671dd07ea5750095c650
marklhor <marklhor@hotmail.fr>
2023-06-21 11:57:35 +0200

    commit, favicon

--- fil-rouge/resources/views/templates/favicon.blade.php
+++ fil-rouge/resources/views/templates/favicon.blade.php
@@ -1,1 +1,11 @@
-<link rel="icon" href="{{ URL::asset('/img/favicon-16x16.ico') }}" type="image/x-icon" />
+{{-- <link rel="icon" href="{{ URL::asset('/img/favicon-16x16.ico') }}" type="image/x-icon" /> --}}
+
+
+<link rel="apple-touch-icon" sizes="180x180" href="{{ URL::asset('/img/favicon/apple-touch-icon.png') }}">
+<link rel="icon" type="image/png" sizes="32x32" href="{{ URL::asset('/img/favicon/favicon-32x32.png') }}">
+<link rel="icon" type="image/png" sizes="16x16" href="{{ URL::asset('/img/favicon/favicon-16x16.png') }}">
+<link rel="apple-touch-icon" sizes="120x120"
+    href="{{ URL::asset('/img/favicon/apple-touch-icon-120x120-precomposed.png') }}" />
+<link rel="apple-touch-icon" sizes="152x152"
+    href="{{ URL::asset('/img/favicon//apple-touch-icon-152x152-precomposed.png') }}" />
+<link rel="manifest" href="{{ URL::asset('/img/favicon/site.webmanifest') }}/site.webmanifest">
--- fil-rouge/resources/views/templates/metas.blade.php
+++ fil-rouge/resources/views/templates/metas.blade.php
@@ -3,5 +3,5 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="author" content="Patrick Nardi">
 <meta name="description"
-    content="AMIO 2isa CDA07 | Projet fil-rouge phase 1 | Gestion des incidents du parc informatique">
+    content="Site de gestion des incidents du parc informatique de l'association AMIO permettant le signalement d’incidents. Dédié aux usagers de la structure">
 <meta name="keywords" content="AMIO, 2isa, CDA07, fil-rouge, incident, informatique">
